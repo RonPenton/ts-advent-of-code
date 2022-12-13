3ad8b6f0e4009add889076a847d4ff1415c55e4a
rlp2 <ron.penton@ellucian.com>
2022-12-13 13:31:51 -0500

    better

--- src/2022/13/02-solution.ts
+++ src/2022/13/02-solution.ts
@@ -6,7 +6,9 @@ type ListItem = number | ListItem[];
 type List = ListItem[];
 type Outcome = 'right' | 'wrong' | 'unknown';
 
-const lines: List[] = text.split(/(?:\r\n){1,2}/im).map(x => JSON.parse(x)).concat([[[2]], [[6]]]);
+const two = [[2]];
+const six = [[6]];
+const lines: List[] = text.split(/(?:\r\n){1,2}/im).map(x => JSON.parse(x)).concat([two, six]);
 
 const validateList = (a: ListItem[], b: ListItem[]): Outcome => {
     if (b.length == 0 && a.length > 0) return 'wrong';
@@ -34,14 +36,6 @@ const sorter = (a: ListItem, b: ListItem): number => {
 
 lines.sort(sorter);
 
-const find = (search: number, depth: number) => (x: ListItem): boolean => {
-    if (typeof x === 'number' && depth == 0 && x == search)
-        return true;
-    if (depth < 0 || typeof x === 'number' || x === undefined)
-        return false;
-    return find(search, depth - 1)(x[0]);
-}
-
-const a = lines.findIndex(find(2, 2)) + 1;
-const b = lines.findIndex(find(6, 2)) + 1;
+const a = lines.indexOf(two) + 1;
+const b = lines.indexOf(six) + 1;
 console.log(a * b);
