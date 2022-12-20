8a16b86165aaea367e88444a2efebab675f4a2c8
rlp2 <ron.penton@ellucian.com>
2022-12-20 09:06:49 -0500

    2022 day 20

---
+++ src/2022/20/01-solution.ts
@@ -0,0 +1,29 @@
+//https://adventofcode.com/2022/day/19
+import { add, fparseInt, readLines } from "../../utils";
+const lines = readLines(`${__dirname}\\01-input.txt`);
+
+const vals = lines.map(fparseInt);
+
+const mix = (a: number[]) => {
+    // array of tuples; we want to keep the index so we can figure out what order to work on them in. 
+    const mixed = a.map((n, i) => [n, i] as const);
+
+    for (let x = 0; x < a.length; x++) {
+        const mi = mixed.findIndex(([_, i]) => i == x);
+        const [n, i] = mixed[mi];
+
+        mixed.splice(mi, 1); // delete the item
+        mixed.splice((mi + n) % mixed.length, 0, [n, i]); // move it and re-add it. Modulo as necessary.
+    }
+
+    return mixed.map(([n]) => n);
+}
+
+
+const mixed = mix(vals);
+
+const idx = (a: number[], i: number) => a[i % a.length];
+
+const zero = mixed.indexOf(0);
+const answer = [1000, 2000, 3000].map(i => idx(mixed, zero + i)).reduce(add);
+console.log(answer);
