7a9ed518a495f8be36ab37579382186d00235558
rlp2 <ron.penton@ellucian.com>
2022-12-25 01:07:46 -0500

    2022 day 25

--- src/2022/25/01-solution.ts
+++ src/2022/25/01-solution.ts
@@ -1,91 +1,49 @@
-//https://adventofcode.com/2022/day/24
-import { addCoord, debug, distCoord, readLines } from "../../utils";
-import { Coord, Direction, eqCoord, Grid, moveCoord, wrapCoord } from "../../utils/grid";
-import PriorityQueue from 'ts-priority-queue';
+//https://adventofcode.com/2022/day/25
+import { add, readLines } from "../../utils";
 
 const lines = readLines(`${__dirname}\\01-input.txt`);
 
-const grid = new Grid<string>(lines.map(x => [...x]));
-
-const start = grid.sliceRows(0, 1).find(x => x == '.')!.coord;
-const end = addCoord(grid.sliceRows(grid.height - 1).find(x => x == '.')!.coord, [grid.height - 1, 0]);
-
-const blizzards = grid.filter(x => x != '.' && x != '#').map(({ coord, val }) => [val, coord] as const);
-type Blizzard = typeof blizzards[number];
-
-const directions: Record<string, Direction> = {
-    '>': 'right',
-    'v': 'down',
-    '<': 'left',
-    '^': 'up'
+const translate: Record<string, number> = {
+    '2': 2,
+    '1': 1,
+    '0': 0,
+    '-': -1,
+    '=': -2
 };
 
-const wrap = wrapCoord(1, 1, grid.height - 1, grid.width - 1);
-const moveBlizzard = ([v, c]: Blizzard): Blizzard => [v, wrap(moveCoord(c, directions[v]))];
-const getCoord = ([_, c]: Blizzard) => c;
+const snafu: Record<number, string> = {
+    [-2]: '=',
+    [-1]: '-',
+    [0]: '0',
+    [1]: '1',
+    [2]: '2'
+};
 
-const patterns = grid.width * grid.height;
-const blizzardLocations: Coord[][] = [blizzards.map(getCoord)];
-let b = blizzards;
-for (let i = 1; i < patterns; i++) {
-    b = b.map(moveBlizzard);
-    blizzardLocations.push(b.map(getCoord));
-}
-
-const Moves = ['up', 'down', 'left', 'right', 'none'] as const;
-type Move = typeof Moves[number];
-
-type State = [Coord, number];
-
-const moves = (player: Coord, turn: number): Move[] => {
-    return Moves.filter(m => {
-        const check = m == 'none' ? player : moveCoord(player, m);
-        const at = grid.atRaw(check);
-        if (at === undefined || at === '#') return false;
-        const bs = blizzardLocations[turn % blizzardLocations.length];
-        if (bs.some(blizzard => eqCoord(blizzard, check)))
-            return false;
-        return true;
-    });
-}
-
-const bfs = (start: Coord, end: Coord) => {
-
-    const heuristic = ([c, d]: State) => d + distCoord(c, end);
-    let looking = new PriorityQueue<State>({
-        comparator: (a, b) => heuristic(a) - heuristic(b),
-        initialValues: [[start, 0]]
-    });
-
-    let memos = new Set<string>();
-    const memoize = ([r, c]: Coord, t: number) => `${r},${c}::${t}`;
-
-    while (true) {
-        const [current, turn] = looking.dequeue();
-        const k = memoize(current, turn);
-        if (memos.has(k)) {
-            continue;
-        }
-
-        memos.add(k);
-        const h = heuristic([current, turn]);
-        debug(`Examining: ${current}, turn: ${turn}, heuristic: ${h}`, 10000);
-
-        const mv = moves(current, turn + 1);
-        for (const m of mv) {
-            if (m == 'none') {
-                looking.queue([current, turn + 1]);
-            }
-            else {
-                const c = moveCoord(current, m);
-                if (eqCoord(c, end)) {
-                    return turn + 1;
-                }
-                looking.queue([c, turn + 1]);
-            }
-        }
+const toSnafu = (s: string) => {
+    const ss = [...s].reverse();
+    let acc = 0;
+    for (let i = 0; i < ss.length; i++) {
+        acc += translate[ss[i]] * Math.pow(5, i);
     }
+    return acc;
 }
 
-const ans = bfs(start, end);
-console.log(ans);
+const fromSnafu = (n: number) => {
+    let sn: string[] = [];
+    while (n) {
+        let r = (n % 5);
+        if (r > 2) {
+            r = r - 5;
+        }
+        n -= r;
+        n /= 5;
+        sn.unshift(snafu[r]);
+    }
+
+    return sn.join('');
+}
+
+const nums = lines.map(toSnafu);
+const sum = nums.reduce(add);
+const val = fromSnafu(sum);
+console.log(val);
