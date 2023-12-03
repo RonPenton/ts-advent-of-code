3935333cf5f3eebad17d1c42a51ad3b4bed4775c
Ron Penton <ron.penton@gmail.com>
2023-12-03 08:17:57 -0500

    day 3 part 2

--- src/2023/03/02-solution.ts
+++ src/2023/03/02-solution.ts
@@ -1,7 +1,62 @@
 // https://adventofcode.com/2023/day/3
 
-import { readLines } from "../../utils";
+import { notEmpty, range, readLines } from "../../utils";
+import { Coord, Directions, Grid, moveCoord, printCoord } from "../../utils/grid";
 
-const lines = readLines(`${__dirname}\\input.txt`);
+const lines = readLines(`${__dirname}\\input.txt`).filter(notEmpty);
 
-console.log(lines);
+const gearMap = new Map<string, number[]>();
+
+const addGear = (key: string, value: number) => {
+    const gears = gearMap.get(key) ?? [];
+    gearMap.set(key, [...gears, value]);
+}
+
+type Digit = '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9';
+type Tile = Digit | 'gear' | null;
+const isDigit = /\d/i;
+
+const grid = Grid.fromLines(lines, s => {
+    if (isDigit.test(s)) return s as Digit;
+    if (s === '*') return 'gear';
+    return null;
+});
+
+const readNumber = (grid: Grid<Tile>, r: number, c: number): string => {
+    let n = '';
+    while (c < grid.width) {
+        const tile = grid.at([r, c]);
+        if (tile === null || tile === 'gear') return n;
+        n += tile;
+        c++;
+    }
+    return n;
+}
+
+const tileAdjacentToGears = (grid: Grid<Tile>, r: number, c: number): Coord[] => {
+    const gearDirections = Directions.filter(d => grid.atDirection([r, c], d) === 'gear');
+    const gearCoordinates = gearDirections.map(d => moveCoord([r, c], d));
+    return gearCoordinates;
+}
+
+const numberAdjacentToGears = (grid: Grid<Tile>, r: number, c: number, number: string): string[] => {
+    const numberCoords = range(number.length).map(i => [r, c + i]);
+    const gearCoords = numberCoords.flatMap(([r, c]) => tileAdjacentToGears(grid, r, c));
+    const unique = [...new Set(gearCoords.map(printCoord))];
+    return unique;
+}
+
+for (let r = 0; r < grid.height; r++) {
+    for (let c = 0; c < grid.width; c++) {
+        const number = readNumber(grid, r, c);
+        if (number.length > 0) {
+            const gearCoords = numberAdjacentToGears(grid, r, c, number);
+            gearCoords.forEach(x => addGear(x, parseInt(number)));
+            c += number.length - 1;
+        }
+    }
+}
+
+const validGears = [...gearMap.entries()].filter(([_, value]) => value.length === 2);
+const sum = validGears.reduce((acc, [_, [a, b]]) => (a * b) + acc, 0);
+console.log(sum);
