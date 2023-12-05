890850bed17a667bea7799e7e1e816abb2e81d70
Ron Penton <ron.penton@gmail.com>
2023-12-05 10:25:49 -0500

    part 2

--- src/2023/05/02-solution.ts
+++ src/2023/05/02-solution.ts
@@ -1,7 +1,83 @@
 // https://adventofcode.com/2023/day/5
 
-import { readLines, notEmpty } from "../../utils";
+import { readLines, notEmpty, iterateRegex, Range, rangeIntersection, rangeDifference, rangesIntersection, rangesOffset, rangesSubtract, rangesUnion } from "../../utils";
 
 const lines = readLines(`${__dirname}\\input.txt`).filter(notEmpty);
 
-console.log(lines);
+const [seedLine, ...rest] = lines;
+
+const seeds = [...iterateRegex(/(\d+) (\d+)/g, seedLine.split(':')[1])]
+    .map(x => [Number(x[1]), Number(x[1]) + Number(x[2])] as const);
+
+// example uses confusing nomenclature.
+// seed = source
+// soil = destination
+
+type SeedRange = {
+    destination: number;
+    source: number;
+    range: number;
+}
+
+type ParsedMap = {
+    from: string;
+    to: string;
+    ranges: SeedRange[];
+}
+
+function* parseMaps(lines: string[]) {
+    let map: ParsedMap | undefined;
+
+    for (const line of lines) {
+        const reg = /(\w+)-to-(\w+) map/i;
+        const match = reg.exec(line);
+        if (match) {
+            if (map) yield map;
+            map = { from: match[1], to: match[2], ranges: [] };
+        }
+        else if (map !== undefined) {
+            const [destination, source, range] = line.split(' ').map(Number);
+            map.ranges.push({ destination, source, range });
+        }
+    }
+
+    if (map) yield map;
+}
+
+const mapRange = (map: ParsedMap, seeds: Range[]): Range[] => {
+    const { ranges } = map;
+    let untestedRanges: Range[] = [...seeds];
+    let results: Range[] = [];
+
+    for (const range of ranges) {
+
+        if (!untestedRanges.length) break;
+
+        const { destination, source, range: r } = range;
+
+        const sourceRange = [source, source + r] as const;
+
+        const intersects = rangesIntersection(untestedRanges, [sourceRange]);
+        if (intersects.length > 0) {
+            results.push(...rangesOffset(intersects, destination - source));
+            untestedRanges = rangesSubtract(untestedRanges, sourceRange);
+        }
+    }
+
+    return rangesUnion([...results, ...untestedRanges]);
+}
+
+const computeRanges = (position: string, maps: Map<string, ParsedMap>, seeds: Range[]): Range[] => {
+    const entry = maps.get(position);
+    if (!entry) return seeds;
+    return computeRanges(entry.to, maps, mapRange(entry, seeds));
+}
+
+
+
+const maps = [...parseMaps(rest)];
+const sourceMap: Map<string, ParsedMap> = new Map(maps.map(m => [m.from, m]));
+
+const solutionRanges = seeds.flatMap(seed => computeRanges('seed', sourceMap, [seed]));
+const lowest = Math.min(...solutionRanges.map(r => r[0]));
+console.log(lowest);
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -41,7 +41,6 @@ export const range = (a: number, b?: number): number[] => {
     return [...Array(b - a).keys()].map(add(a));
 }
 
-export const between = ([a, b]: [number, number]) => (n: number) => n >= a && n <= b;
 
 export function apply<T extends unknown[], U extends unknown[], R>(fn: (...args: [...T, ...U]) => R, ...front: T) {
     return (...tailArgs: U) => fn(...front, ...tailArgs);
@@ -50,6 +49,75 @@ export function apply<T extends unknown[], U extends unknown[], R>(fn: (...args:
 export const arrayFrom = (str: string) => Array.from(str);
 
 export type Coord = [r: number, c: number];
+export type Range = readonly [number, number];
+
+
+export const between = ([a, b]: Range) => (n: number) => n >= a && n <= b;
+export const rangeIntersection = ([a, b]: Range, [c, d]: Range): Range | undefined => {
+    if (a > d || c > b) return undefined;
+    return [Math.max(a, c), Math.min(b, d)];
+}
+export const rangeDifference = ([a, b]: Range, [c, d]: Range): Range[] => {
+    if (a > d || c > b) return [[a, b]];
+    if (a < c && b > d) return [[a, c - 1], [d + 1, b]];
+    if (a < c) return [[a, c - 1]];
+    if (b > d) return [[d + 1, b]];
+    return [];
+}
+export const rangeUnion = ([a, b]: Range, [c, d]: Range): Range[] => {
+    if (a > d || c > b) return [[a, b], [c, d]];
+    return [[Math.min(a, c), Math.max(b, d)]];
+}
+
+export const rangesSubtract = (ranges: Range[], toRemove: Range): Range[] => {
+    const result: Range[] = [];
+    for (const range of ranges) {
+        const diff = rangeDifference(range, toRemove);
+        result.push(...diff);
+    }
+    return rangesUnion(result);
+}
+export const rangesUnion = (ranges: Range[]): Range[] => {
+    const sorted = ranges.sort((a, b) => a[0] - b[0]);
+    const result: Range[] = [];
+    let current = sorted[0];
+    for (let i = 1; i < sorted.length; i++) {
+        const next = sorted[i];
+        const union = rangeUnion(current, next);
+        if (union.length === 1) {
+            current = union[0];
+        }
+        else {
+            result.push(current);
+            current = next;
+        }
+    }
+    if (current)
+        result.push(current);
+    return result;
+}
+
+export const rangesIntersection = (ranges: Range[], intersections: Range[]): Range[] => {
+    const result: Range[] = [];
+    for (const range of ranges) {
+        for (const intersection of intersections) {
+            const i = rangeIntersection(range, intersection);
+            if (i) {
+                result.push(i);
+            }
+        }
+    }
+    return rangesUnion(result);
+}
+
+export const rangeOffset = (range: Range, offset: number): Range => {
+    const [a, b] = range;
+    return [a + offset, b + offset];
+}
+export const rangesOffset = (ranges: Range[], offset: number): Range[] => {
+    return ranges.map(r => rangeOffset(r, offset));
+}
+
 export type GridPredicate<T, R = void> = (r: number, c: number, val: T, grid: T[][]) => R;
 export function gridForEach<T>(g: T[][], f: GridPredicate<T>) {
     g.forEach((row, r) => row.forEach((val, c) => f(r, c, val, g)));
