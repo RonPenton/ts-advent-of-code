f85ec12c0f6ce2f8e7fa2fe4bdb789fb6213ebc3
Ron Penton <ron.penton@gmail.com>
2023-12-09 08:41:47 -0500

    cleaner

--- src/2023/09/02-solution.ts
+++ src/2023/09/02-solution.ts
@@ -1,19 +1,18 @@
 // https://adventofcode.com/2023/day/9
 
 import { readLines, notEmpty, add, defined } from "../../utils";
+import { iter } from "../../utils/iter";
 
 const lines = readLines(`${__dirname}\\input.txt`)
     .filter(notEmpty)
     .map(x => x.split(' ').map(Number));
 
 const deriveNext = (numbers: number[]): number => {
-
-    if(numbers.every(x => x == 0))
-        return 0;
-
-    const differences = numbers
-        .map((x, i) => i == 0 ? undefined : x - numbers[i - 1])
-        .filter(defined);
+    if (numbers.every(x => x == 0)) return 0;
+    const differences = iter(numbers)
+        .skip(1)
+        .map((x, i) => x - numbers[i])
+        .array();
 
     return numbers[0] - deriveNext(differences);
 }
