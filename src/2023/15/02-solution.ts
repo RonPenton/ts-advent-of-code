2aebe58d779bf75fe50c81b1dfc8f9fc1f6d3360
Ron Penton <ron.penton@gmail.com>
2023-12-15 08:16:46 -0500

    day 2

--- src/2023/15/02-solution.ts
+++ src/2023/15/02-solution.ts
@@ -1,7 +1,52 @@
 // https://adventofcode.com/2023/day/15
 
-import { readLines, notEmpty } from "../../utils";
+import { readLines, notEmpty, add } from "../../utils";
 
-const lines = readLines(`${__dirname}\\input.txt`);
+const lines = readLines(`${__dirname}\\input.txt`).filter(notEmpty);
 
-console.log(lines);
+type Lens = { code: string, value: number };
+type Input = Lens | { code: string; }
+
+const parseCode = (str: string): Input => {
+    if (str.includes('=')) {
+        const [code, value] = str.split('=');
+        return { code, value: Number(value) };
+    }
+    return { code: str.replace('-', '') };
+}
+
+
+const inputs = lines.flatMap(line => line.split(',')).map(parseCode);
+
+const hashReduce = (acc: number, b: string): number => (17 * (acc + b.charCodeAt(0))) & 0xff;
+const hash = (s: string): number => s.split('').reduce(hashReduce, 0);
+
+const map = new Map<number, Lens[]>();
+
+for (const input of inputs) {
+    const h = hash(input.code);
+    if ('value' in input) {
+        const lenses = map.get(h) ?? [];
+        const lens = lenses.find(x => x.code === input.code);
+        if (lens)
+            lens.value = input.value;
+        else
+            lenses.push(input);
+        map.set(h, lenses);
+    } else {
+        const lenses = map.get(h) ?? [];
+        map.set(h, lenses.filter(x => x.code !== input.code));
+    }
+}
+
+const focusingPower = (map: Map<number, Lens[]>): number => {
+    let power = 0;
+    for (const [box, lenses] of map.entries()) {
+        power += (box+1) * lenses.reduce((acc, { value }, i) => acc + (value * (i + 1)), 0);
+    }
+    return power;
+}
+
+
+console.log(focusingPower(map));
+
