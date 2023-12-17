2836f742f8a9fffa58b0dc0eaad5e882b0f05d43
Ron Penton <ron.penton@gmail.com>
2023-12-17 15:14:51 -0500

    2020 13 1

---
+++ src/2020/13/01-solution.ts
@@ -0,0 +1,20 @@
+
+import { readLines, notEmpty } from "../../utils";
+
+const lines = readLines(`${__dirname}\\input.txt`);
+const [a, b] = lines;
+
+const time = Number(a);
+const busses = b.split(',').filter(x => x !== 'x').map(Number);
+
+let t = time;
+while(true) {
+    const bus = busses.find(x => t % x === 0);
+    if(bus) {
+        console.log(bus);
+        console.log(t);
+        console.log(bus * (t - time));
+        break;
+    }
+    t++;
+}
